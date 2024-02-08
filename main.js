a5d9dc1f0943b264b52a6580e914db7bff859622
antonfed <fed.anton@gmail.com>
2024-02-08 10:27:22 +0100

    dive in

--- main.js
+++ main.js
@@ -71,7 +71,7 @@ class GlitchPass extends Pass {
 			this.curF = 0;
 			this.generateTrigger();
 
-		} else if ( this.curF % this.randX < this.randX / 15 ) {
+		} else if ( this.curF % this.randX < this.randX / 30 ) {
 
 			this.uniforms[ 'amount' ].value = Math.random() / 300;
 			this.uniforms[ 'angle' ].value = MathUtils.randFloat( - Math.PI, Math.PI );
